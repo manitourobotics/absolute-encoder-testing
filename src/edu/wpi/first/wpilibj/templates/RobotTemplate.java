dc63f302505360586707d784df5bb7dcc82f0591
Bryce Guinta <azrathud@gmail.com>
2013-02-17 14:36:43 -0700

    add wiring instructions

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -6,12 +6,22 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.SimpleRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the SimpleRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
+/*
+ * This class is used to test out the AS5145B encodder for absolute input
+ * via the pwm out put through a low-pass filter
+ * See the datasheet for instructions for wiring:
+ * http://www.ams.com/eng/content/download/50206/533867/34237
+ * From:
+ * http://www.ams.com/eng/Products/Magnetic-Position-Sensors/Magnetic-Rotary-Position-Sensors/AS5145B
+ * Wiring:
+ * one wire connected to a ground(GND pin) on the magnetic encoder to the analog breakout gnd(-)
+ * one wire going from the analog breakout power(pwr) to the 5v pin on the encoder
+ * one wire going from the PWM pin on the encoder to a low-pass filter to change the PWM output to analog and then from the low pass filter to the desired channel on the analog breakout on the cRIO
+ * optional wiring:
+ * one wire to each the Mag DECn pin and the Mag INCn for testing to a pin on the digital sidecar 
+ * see section 4.1 of the manual for pin descriptions 
+ * 
+ * 
  */
 public class RobotTemplate extends SimpleRobot {
 
